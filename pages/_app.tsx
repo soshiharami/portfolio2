ae80878d1680f53cd13e44a0650e9b43d8dc5f51
soshiharami <soshi0807sss@gmail.com>
2021-05-28 18:36:13 +0900

    init commit

---
+++ pages/_app.tsx
@@ -0,0 +1,19 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client'; 
+
+const client = new ApolloClient({
+  uri: 'https://portfolioapi.soshiharami.com',
+  cache: new InMemoryCache()
+});
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+  );
+}
+
+export default MyApp

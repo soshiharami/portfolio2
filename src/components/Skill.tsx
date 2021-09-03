0acad65edf37c8b981278d272816161bfce3a1d8
soshiharami <soshi0807sss@gmail.com>
2021-09-04 02:46:51 +0900

    feat: create skill components

---
+++ src/components/Skill.tsx
@@ -0,0 +1,15 @@
+import React, {FC} from "react";
+import {skill} from "../types/Skill";
+import style from "../styles/Skill.module.css"
+
+interface props {
+    skill: skill
+}
+
+export const Skill: FC<props> = ({skill}) => {
+    return (
+        <div className={style.skill}>
+            <div>{skill.name}</div>
+        </div>
+    )
+}
